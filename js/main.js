adc91c63bbe02cb37ebac41c89288519260ffa54
Alexandre Le Foll <alefoll@fdgueux.fr>
2015-01-18 19:03:04 +0100

    Handle close tray only one screen (Keywords)

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var ipc = require('ipc');
+
+window.addEventListener('polymer-ready', function(evt) {
+	var core = document.querySelector('core-selector');
+
+	ipc.on('key', function(message) {
+		if (message === 'MediaPlayPause')
+			core.selected = "0";
+		else if (message === 'MediaNextTrack')
+			core.selected = parseInt(core.selected) + 1;
+		else if (message === 'MediaPreviousTrack')
+			core.selected = parseInt(core.selected) - 1;
+	});
+});
+
+window.onbeforeunload = function(e) {
+	return false;
+};
--- main.js
+++ main.js
@@ -2,42 +2,58 @@ var app  = require('app'),
 	Menu = require('menu'),
 	Tray = require('tray');
 
-var BrowserWindow = require('browser-window');
+var BrowserWindow  = require('browser-window'),
+	GlobalShortcut = require('global-shortcut');
 
 // Report crashes to our server.
 require('crash-reporter').start();
 
-var mainWindow = null;
+var window = null;
 var appIcon = null;
 
 app.on('window-all-closed', function() {
-	if (process.platform != 'darwin')
-		app.quit();
+	console.log('all-closed');
 });
 
 app.on('ready', function() {
 	var size = require('screen').getPrimaryDisplay().workAreaSize;
 
-	// mainWindow = new BrowserWindow({ width: size.width, height: size.height, frame: false });
-	mainWindow = new BrowserWindow({ width: 800, height: 600, frame: false });
+	window = new BrowserWindow({ width: size.width, height: size.height, frame: false });
 
-	mainWindow.loadUrl('file://' + __dirname + '/index.html');
+	window.toggleDevTools();
+	window.loadUrl('file://' + __dirname + '/index.html');
 
-	mainWindow.on('closed', function() {
-		mainWindow = null;
+	window.on('close', function() {
+		window.hide();
 	});
 
 	appIcon = new Tray('./web.png');
 
 	var contextMenu = Menu.buildFromTemplate([
-		{ label: 'Dev Tools', click: function() { mainWindow.toggleDevTools(); } },
-		{ label: 'Exit', click: function() { mainWindow.close(); } }
+		{ label: 'Dev Tools', click: function() { window.toggleDevTools(); } },
+		{ label: 'Exit', click: function() { window.destroy(); GlobalShortcut.unregisterAll(); app.quit(); } }
 	]);
 
 	appIcon.on('clicked', function() {
-		mainWindow.focus();
+		window.show();
 	});
 
 	appIcon.setToolTip('Podcast');
 	appIcon.setContextMenu(contextMenu);
+
+	GlobalShortcut.register('F5', function() {
+		window.webContents.reload();
+	});
+
+	GlobalShortcut.register('MediaPlayPause', function() {
+		window.webContents.send('key', 'MediaPlayPause');
+	});
+
+	GlobalShortcut.register('MediaNextTrack', function() {
+		window.webContents.send('key', 'MediaNextTrack');
+	});
+
+	GlobalShortcut.register('MediaPreviousTrack', function() {
+		window.webContents.send('key', 'MediaPreviousTrack');
+	});
 });
